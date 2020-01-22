a50e43d342c54e2c05a03c5ece86868278f663d8
Jorge Cimentada <cimentadaj@gmail.com>
2020-01-22 16:18:52 +0100

    Initial commit for ridge -- nearly finished with first Rmarkdown tutorial

---
+++ data/transform_data.R
@@ -0,0 +1,29 @@
+library(haven)
+library(recipes)
+library(here)
+library(readr)
+library(dplyr)
+
+pisa <- read_spss(here("data", "pisa_2018.sav"))
+
+pisa_us <-
+  pisa %>%
+  filter(CNT == "USA") %>% 
+  mutate_if(is.labelled, zap_labels) %>% 
+  mutate_at(vars(MISCED, FISCED, REPEAT, IMMIG, DURECEC), as.character)
+
+# Calculate the average test score in mathematics as the average of all
+# 10 plausible columns
+pv_ind <- grepl("PV[0-9]MATH", names(pisa_us))
+pisa_us$math_score <- rowMeans(pisa_us[pv_ind])
+
+pisa_us_ready <-
+  pisa_us %>% 
+  recipe(math_score ~ ., data = .) %>%
+  step_zv(all_predictors()) %>% 
+  step_meanimpute(all_predictors(), -all_nominal()) %>% 
+  step_modeimpute(all_predictors(), -all_numeric()) %>%
+  prep() %>%
+  juice()
+
+write_csv(pisa_us_ready, here("data", "pisa_us_2018.csv"))
