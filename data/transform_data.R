228e9f71df788f884629fa6275eff0df927efbf9
Jorge Cimentada <cimentadaj@gmail.com>
2020-01-29 13:47:53 +0100

    Updates the US pisadata to include non cognitive variable

--- data/transform_data.R
+++ data/transform_data.R
@@ -17,14 +17,24 @@ pisa_us <-
 pv_ind <- grepl("PV.+MATH", names(pisa_us))
 pisa_us$math_score <- rowMeans(pisa_us[pv_ind])
 
+noncogn_questions <- c("ST182Q03HA",
+                       "ST182Q04HA",
+                       "ST182Q05HA",
+                       "ST182Q06HA")
+
+pisa_us$noncogn <- rowMeans(pisa_us[noncogn_questions], na.rm = TRUE)
+
 pisa_us_ready <-
   pisa_us %>%
-  select(-matches("PV.+MATH")) %>% 
+  # WORKMAST is a index that has the noncogn questions
+  select(-matches("PV.+MATH"), -noncogn_questions, -WORKMAST, -COMPETE) %>%
   recipe(math_score ~ ., data = .) %>%
-  step_zv(all_predictors()) %>% 
+  step_zv(all_predictors()) %>%
+  step_nzv(all_predictors()) %>% 
   step_meanimpute(all_predictors(), -all_nominal()) %>% 
   step_modeimpute(all_predictors(), -all_numeric()) %>%
   prep() %>%
   juice()
 
 write_csv(pisa_us_ready, here("data", "pisa_us_2018.csv"))
+
