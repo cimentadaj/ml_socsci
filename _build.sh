cbb91c3015b0c7a90b70919fcaee354e5c7d4f53
Jorge Cimentada <cimentadaj@gmail.com>
2020-05-29 18:15:07 +0200

    ...

--- _build.sh
+++ _build.sh
@@ -2,6 +2,6 @@
 
 set -ev
 
-Rscript -e "install.packages('renv'); renv::restore(); bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+Rscript -e "install.packages('renv'); renv::status(); bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 # Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 # Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
